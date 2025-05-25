ec25646308ad59c44e8a82ef63c3b906b45b7984
jack <jackdixontas@gmail.com>
2025-05-25 14:30:27 +1000

    Can Play multiple games

--- connect_four.py
+++ connect_four.py
@@ -5,8 +5,6 @@ player_in = ""
 
 valid_commands = ["1","2","3","4","5","6","7","e","exit"]
 
-print("Starting game...")
-
 game_board = Board()
 
 
@@ -14,38 +12,76 @@ game_board = Board()
 def is_valid_input(_in):
     return _in in valid_commands
 
+running_game = True
 
-#get inputs
-while True:
+while running_game:
+    print("Starting new game...")
+    #selecting your opponent
+    playing_game = True
+    while True:
 
-    #playing game
-    game_board.print_board()
+        print("""Select Opponent:
+    1. Human (same machine)
+    2. Easy AI
+    3. Normal AI
+    4. Hard AI
+e/exit: stop playing""")
+        
+        
+        match (input("Enter Opponent: ").strip().lower()):
+            case "1": 
+                break
+            case "2": 
+                break
+            case "3": 
+                break
+            case "4": 
+                break
+            case "e": 
+                playing_game = False
+                break
+            case "-":
+                continue
     
-
-
-    player_in = input("Enter your move:").strip().lower()
-
-    if not is_valid_input(player_in):
-        print("Invalid input")
-        continue
-
-    if player_in in ["e", "exit"]:
-        print("Exiting...")
-        break
+        
     
-    if game_board.is_move_valid(int(player_in) - 1):
-        game_board.play_move(int(player_in) - 1)
-        winner = game_board.check_game_win()
+    #Playing the game
+    while playing_game:
+        #playing game
+        game_board.print_board()
+        
+        turn = game_board.get_current_turn()
 
-        if winner != None:
-            game_board.print_board()
-            print(winner + " Wins!")
+        player_in = input(turn+"'s turn\nEnter a move:").strip().lower()
+
+        if not is_valid_input(player_in):
+            print("Invalid input")
+            continue
+
+        if player_in in ["e", "exit"]:
+            print("Exiting...")
             break
+        
+        if game_board.is_move_valid(int(player_in) - 1):
+            game_board.play_move(int(player_in) - 1)
+            winner = game_board.check_game_win()
+
+            if winner != None:
+                game_board.print_board()
+                print(winner + " Wins!")
+                break
     
 
-    
-    
+    #Game is over
+    if playing_game:
+        print("Game over")
+        #print score here if needed
+
+        #Ask to play another game
+
+
+    else:
+        running_game = False
 
     
 
-print("Game over")
